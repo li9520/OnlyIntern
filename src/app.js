2c11a92138d7b1f4f3f23f95bad871a46b9db17c
li9520 <li.dp9520@gmail.com>
2023-08-30 18:14:35 +0500

    fix

--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
 
+const renderPoints = (container, state) => {
+  container.innerHTML = '';
+  state.points.forEach(({id, name, location, backgroundColor}) => {
+    const isActive = state.activePoints.includes(id);
+    const point = document.createElement('button');
+    const [top, left] = location;
+    point.dataset.id = id;
+    point.dataset.name = name;
+    point.classList.add('point');
+    if(isActive) {
+      point.classList.add('active');
+    }
+    point.innerHTML = isActive ?  `<div>&minus;</div><div>${name}</div>`: '&plus;';
+    point.style.top = top;
+    point.style.left = left;
+    point.style.backgroundColor = backgroundColor;
+    container.append(point);
+  })
+}
+
 const app = (defaultPoints=[]) => {
   const state = {
     points: defaultPoints,
     activePoints: [],
   }
   const container = document.querySelector('.main');
-
-  const renderPoints = (container, state) => {
-    container.innerHTML = '';
-    state.points.forEach(({id, name, location, backgroundColor}) => {
-      const isActive = state.activePoints.includes(id);
-      const point = document.createElement('button');
-      const [top, left] = location;
-      point.dataset.id = id;
-      point.dataset.name = name;
-      point.classList.add('point');
-      if(isActive) {
-        point.classList.add('active');
-      }
-      point.innerHTML = isActive ?  `<div>&minus;</div><div>${name}</div>`: '&plus;';
-      point.style.top = top;
-      point.style.left = left;
-      point.style.backgroundColor = backgroundColor;
-      container.append(point);
-    })
-  }
 
   container.addEventListener('click', (e) => {
     const button = e.target.closest('button');
@@ -40,7 +40,7 @@ const app = (defaultPoints=[]) => {
     }
     renderPoints(container, state)
   })
-  
+
   renderPoints(container, state);
 }
 
